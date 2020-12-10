85715d9b9171b301300eb8f46aa7cf6c064a9aee
Stephen Tan <sttan@ucsd.edu>
2020-12-09 17:06:07 -0800

    Users properly display on search

---
+++ WorkoutBuddies/Cells/SearchCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchCell.swift
+//  WorkoutBuddies
+//
+//  Created by Stephen Tan on 12/9/20.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var countWorkoutsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- WorkoutBuddies/ViewControllers/SearchViewController.swift
+++ WorkoutBuddies/ViewControllers/SearchViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import Parse
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var users = [PFObject]()
+    var currUser = PFUser()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,17 +21,39 @@ class SearchViewController: UIViewController {
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        currUser = PFUser.current()!
+        getUsers()
+    }
+    
+    func getUsers() {
+        let query = PFUser.query()
+        query?.limit = 20
+        query?.whereKey("objectId", notEqualTo: currUser)
+        self.users = try! query!.findObjects()
+        tableView.reloadData()
     }
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as! SearchCell
+        let potentialBuddy = users[indexPath.row] as! PFUser
+        
+        if potentialBuddy["profileImage"] != nil {
+            let imageFile = potentialBuddy["profileImage"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.userImageView.af.setImage(withURL: url)
+        }
+        
+        cell.usernameLabel.text = potentialBuddy["username"] as? String
+        cell.nameLabel.text = potentialBuddy["name"] as? String
+        
+        return cell
     }
-    
-    
 }
